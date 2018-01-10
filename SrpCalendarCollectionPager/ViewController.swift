6dad77d6f495c4624cea6f925706c59f50bfc0ca
Manikanta <manikanta@manikantas-macbook-pro.local>
2018-01-10 18:34:02 +0530

    Initial Commit

---
+++ SrpCalendarCollectionPager/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SrpCalendarCollectionPager
+//
+//  Created by Manikanta on 1/10/18.
+//  Copyright © 2018 Manikanta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
